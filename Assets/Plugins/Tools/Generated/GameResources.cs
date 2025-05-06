44bb07a174dd9a1f07dbb029040fde1891e8a94d
Screeki <nvbtds@gmail.com>
2025-05-06 15:55:28 +0500

    Add packages and tools

---
+++ Assets/Plugins/Tools/Generated/GameResources.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+// This file is auto-generated. Do not modify manually.
+
+public static class GameResources
+{
+    public static class Prefabs
+    {
+    }
+    public static class Textures
+    {
+        public static Sprite square_borders => Resources.Load<Sprite>("Textures/square_borders");
+    }
+}
